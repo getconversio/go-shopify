39600ea2e509c479f93394a57e3ed60706ae17b0
David Volquartz Lebech <david@lebech.info>
2016-05-12 10:24:33 +0200

    Add error handling

--- goshopify_test.go
+++ goshopify_test.go
@@ -1,7 +1,9 @@
 package goshopify
 
 import (
+	"fmt"
 	"io/ioutil"
+	"net/http"
 	"reflect"
 	"testing"
 
@@ -87,22 +89,114 @@ func TestDo(t *testing.T) {
 	setup()
 	defer teardown()
 
-	httpmock.RegisterResponder("GET", "https://fooshop.myshopify.com",
-		httpmock.NewStringResponder(200, `{"Foo":"bar"}`))
-
-	type foo struct {
-		Foo string
+	type MyStruct struct {
+		Foo string `json:"foo"`
 	}
 
-	req, _ := client.NewRequest("GET", "", nil)
-	body := new(foo)
-	err := client.Do(req, body)
-	if err != nil {
-		t.Fatalf("Do(): %v", err)
+	cases := []struct {
+		url       string
+		responder httpmock.Responder
+		expected  interface{}
+	}{
+		{
+			"foo/1",
+			httpmock.NewStringResponder(200, `{"foo": "bar"}`),
+			&MyStruct{Foo: "bar"},
+		},
+		{
+			"foo/2",
+			httpmock.NewStringResponder(404, `{"error": "does not exist"}`),
+			ResponseError{Status: 404, Message: "does not exist"},
+		},
+		{
+			"foo/3",
+			httpmock.NewStringResponder(400, `{"errors": {"title": ["wrong"]}}`),
+			ResponseError{Status: 400, Message: "wrong", Errors: []string{"title: wrong"}},
+		},
 	}
 
-	expected := &foo{"bar"}
-	if !reflect.DeepEqual(body, expected) {
-		t.Errorf("Response body = %v, expected %v", body, expected)
+	for _, c := range cases {
+		shopUrl := fmt.Sprintf("https://fooshop.myshopify.com/%v", c.url)
+		httpmock.RegisterResponder("GET", shopUrl, c.responder)
+
+		body := new(MyStruct)
+		req, _ := client.NewRequest("GET", c.url, nil)
+		err := client.Do(req, body)
+
+		if err != nil && !reflect.DeepEqual(err, c.expected) {
+			t.Errorf("Do(): expected error %#v, actual %#v", c.expected, err)
+		} else if err == nil && !reflect.DeepEqual(body, c.expected) {
+			t.Errorf("Do(): expected %#v, actual %#v", c.expected, body)
+		}
+	}
+}
+
+func TestResponseErrorError(t *testing.T) {
+	cases := []struct {
+		err      ResponseError
+		expected string
+	}{
+		{
+			ResponseError{Message: "oh no"},
+			"oh no",
+		},
+		{
+			ResponseError{},
+			"Unknown Error",
+		},
+		{
+			ResponseError{Errors: []string{"title: not a valid title"}},
+			"title: not a valid title",
+		},
+		{
+			ResponseError{Errors: []string{
+				"not a valid title",
+				"not a valid description",
+			}},
+			// The strings are sorted description comes first
+			"not a valid description, not a valid title",
+		},
+	}
+
+	for _, c := range cases {
+		actual := fmt.Sprint(c.err)
+		if actual != c.expected {
+			t.Errorf("ResponseError.Error(): expected %s, actual %s", c.expected, actual)
+		}
+	}
+}
+
+func TestCheckResponseError(t *testing.T) {
+	cases := []struct {
+		resp     *http.Response
+		expected error
+	}{
+		{
+			httpmock.NewStringResponse(200, `{"foo": "bar"}`),
+			nil,
+		},
+		{
+			httpmock.NewStringResponse(299, `{"foo": "bar"}`),
+			nil,
+		},
+		{
+			httpmock.NewStringResponse(400, `{"error": "bad request"}`),
+			ResponseError{Status: 400, Message: "bad request"},
+		},
+		{
+			httpmock.NewStringResponse(500, `{"error": "terrible error"}`),
+			ResponseError{Status: 500, Message: "terrible error"},
+		},
+		{
+			httpmock.NewStringResponse(400, `{"errors": { "order": ["order is wrong"] }}`),
+			ResponseError{Status: 400, Message: "order is wrong", Errors: []string{"order: order is wrong"}},
+		},
+	}
+
+	for _, c := range cases {
+		actual := CheckResponseError(c.resp)
+		if !reflect.DeepEqual(actual, c.expected) {
+			t.Errorf("CheckResponseError(): expected %#v, actual %#v", c.expected, actual)
+		}
 	}
 }
