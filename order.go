d9c2efc3c0282ed3678161c6ea0153d999ea78f0
David Volquartz Lebech <david@lebech.info>
2016-05-17 13:38:25 +0200

    Fix wrong latitude/longitude types

--- order.go
+++ order.go
@@ -77,22 +77,22 @@ type Order struct {
 }
 
 type Address struct {
-	ID           int    `json:"id"`
-	Address1     string `json:"address1"`
-	Address2     string `json:"address2"`
-	City         string `json:"city"`
-	Company      string `json:"company"`
-	Country      string `json:"country"`
-	CountryCode  string `json:"country_code"`
-	FirstName    string `json:"first_name"`
-	LastName     string `json:"last_name"`
-	Latitude     string `json:"latitude"`
-	Longitude    string `json:"longitude"`
-	Name         string `json:"name"`
-	Phone        string `json:"phone"`
-	Province     string `json:"province"`
-	ProvinceCode string `json:"province_code"`
-	Zip          string `json:"zip"`
+	ID           int     `json:"id"`
+	Address1     string  `json:"address1"`
+	Address2     string  `json:"address2"`
+	City         string  `json:"city"`
+	Company      string  `json:"company"`
+	Country      string  `json:"country"`
+	CountryCode  string  `json:"country_code"`
+	FirstName    string  `json:"first_name"`
+	LastName     string  `json:"last_name"`
+	Latitude     float64 `json:"latitude"`
+	Longitude    float64 `json:"longitude"`
+	Name         string  `json:"name"`
+	Phone        string  `json:"phone"`
+	Province     string  `json:"province"`
+	ProvinceCode string  `json:"province_code"`
+	Zip          string  `json:"zip"`
 }
 
 type DiscountCode struct {
