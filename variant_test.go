5976eacec4c5b571ce7b0c9b72669047fbb0b495
Bryan Zegar <bryanzegar@Bryans-Mac-mini.local>
2017-10-16 14:11:07 -0400

    Added test for Variant.Count

--- variant_test.go
+++ variant_test.go
@@ -3,6 +3,7 @@ package goshopify
 import (
 	"reflect"
 	"testing"
+	"time"
 
 	httpmock "gopkg.in/jarcoal/httpmock.v1"
 )
@@ -22,5 +23,37 @@ func TestVariantList(t *testing.T) {
 	expected := []Variant{{ID: 1}, {ID: 2}}
 	if !reflect.DeepEqual(variants, expected) {
 		t.Errorf("Variant.List returned %+v, expected %+v", variants, expected)
+	}
+}
+
+func TestVariantCount(t *testing.T) {
+	setup()
+	defer teardown()
+
+	httpmock.RegisterResponder("GET", "https://fooshop.myshopify.com/admin/products/1/variants/count.json",
+		httpmock.NewStringResponder(200, `{"count": 3}`))
+
+	httpmock.RegisterResponder("GET", "https://fooshop.myshopify.com/admin/products/1/variants/count.json?created_at_min=2016-01-01T00%3A00%3A00Z",
+		httpmock.NewStringResponder(200, `{"count": 2}`))
+
+	cnt, err := client.Variant.Count(nil)
+	if err != nil {
+		t.Errorf("Variant.Count returned error: %v", err)
+	}
+
+	expected := 3
+	if cnt != expected {
+		t.Errorf("Variant.Count returned %d, expected %d", cnt, expected)
+	}
+
+	date := time.Date(2016, time.January, 1, 0, 0, 0, 0, time.UTC)
+	cnt, err = client.Variant.Count(CountOptions{CreatedAtMin: date})
+	if err != nil {
+		t.Errorf("Product.Count returned %d, expected %d", cnt, expected)
+	}
+
+	expected = 2
+	if cnt != expected {
+		t.Errorf("Variant.Count returned %d, expected %d", cnt, expected)
 	}
 }
