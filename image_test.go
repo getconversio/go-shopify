e2788953fd6e26551021e5e29c6384c7d899ad5d
Bryan Zegar <bryanzegar@Bryans-Mac-mini.local>
2017-10-23 20:02:25 -0400

    Added test for Image.List

---
+++ image_test.go
@@ -0,0 +1,90 @@
+package goshopify
+
+import (
+	"testing"
+	"time"
+
+	"gopkg.in/jarcoal/httpmock.v1"
+)
+
+func imageTests(t *testing.T, image Image) {
+	// Check that ID is set
+	expectedImageID := 1
+	if image.ID != expectedImageID {
+		t.Errorf("Image.ID returned %+v, expected %+v", image.ID, expectedImageID)
+	}
+
+	// Check that product_id is set
+	expectedProductID := 1
+	if image.ProductID != expectedProductID {
+		t.Errorf("Image.ProductID returned %+v, expected %+v", image.ProductID, expectedProductID)
+	}
+
+	// Check that position is set
+	expectedPosition := 1
+	if image.Position != expectedPosition {
+		t.Errorf("Image.Position returned %+v, expected %+v", image.Position, expectedPosition)
+	}
+
+	// Check that width is set
+	expectedWidth := 123
+	if image.Width != expectedWidth {
+		t.Errorf("Image.Width returned %+v, expected %+v", image.Width, expectedWidth)
+	}
+
+	// Check that height is set
+	expectedHeight := 456
+	if image.Height != expectedHeight {
+		t.Errorf("Image.Height returned %+v, expected %+v", image.Height, expectedHeight)
+	}
+
+	// Check that src is set
+	expectedSrc := "https://cdn.shopify.com/s/files/1/0006/9093/3842/products/ipod-nano.png?v=1500937783"
+	if image.Src != expectedSrc {
+		t.Errorf("Image.Src returned %+v, expected %+v", image.Src, expectedSrc)
+	}
+
+	// Check that variant ids are set
+	expectedVariantIds := make([]int, 2)
+	expectedVariantIds[0] = 808950810
+	expectedVariantIds[1] = 808950811
+
+	if image.VariantIds[0] != expectedVariantIds[0] {
+		t.Errorf("Image.VariantIds[0] returned %+v, expected %+v", image.VariantIds[0], expectedVariantIds[0])
+	}
+	if image.VariantIds[1] != expectedVariantIds[1] {
+		t.Errorf("Image.VariantIds[0] returned %+v, expected %+v", image.VariantIds[1], expectedVariantIds[1])
+	}
+
+	// Check that CreatedAt date is set
+	expectedCreatedAt := time.Date(2017, time.July, 24, 19, 9, 43, 0, time.UTC)
+	if !expectedCreatedAt.Equal(*image.CreatedAt) {
+		t.Errorf("Image.CreatedAt returned %+v, expected %+v", image.CreatedAt, expectedCreatedAt)
+	}
+
+	// Check that UpdatedAt date is set
+	expectedUpdatedAt := time.Date(2017, time.July, 24, 19, 9, 43, 0, time.UTC)
+	if !expectedUpdatedAt.Equal(*image.UpdatedAt) {
+		t.Errorf("Image.UpdatedAt returned %+v, expected %+v", image.UpdatedAt, expectedUpdatedAt)
+	}
+}
+
+func TestImageList(t *testing.T) {
+	setup()
+	defer teardown()
+
+	httpmock.RegisterResponder("GET", "https://fooshop.myshopify.com/admin/products/1/images.json",
+		httpmock.NewBytesResponder(200, loadFixture("images.json")))
+
+	images, err := client.Image.List(1, nil)
+	if err != nil {
+		t.Errorf("Images.List returned error: %v", err)
+	}
+
+	// Check that images were parsed
+	if len(images) != 2 {
+		t.Errorf("Image.List got %v images, expected 2", len(images))
+	}
+
+	imageTests(t, images[0])
+}
