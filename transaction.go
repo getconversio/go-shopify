05ae193847b447ad058844e559346a099bf4a580
sudomake <ca8e8e5e@opayq.com>
2017-12-18 18:30:06 -0500

    Added Transaction.List

---
+++ transaction.go
@@ -0,0 +1,29 @@
+package goshopify
+
+import "fmt"
+
+// TransactionService is an interface for interfacing with the transactions endpoints of
+// the Shopify API.
+// See: https://help.shopify.com/api/reference/transaction
+type TransactionService interface {
+	List(int, interface{}) ([]Transaction, error)
+}
+
+// TransactionServiceOp handles communication with the transaction related methods of the
+// Shopify API.
+type TransactionServiceOp struct {
+	client *Client
+}
+
+// Represents the result from the transactions.json endpoint
+type TransactionsResource struct {
+	Transactions []Transaction `json:"transactions"`
+}
+
+// List transactions
+func (s *TransactionServiceOp) List(orderID int, options interface{}) ([]Transaction, error) {
+	path := fmt.Sprintf("%s/%d/transactions.json", ordersBasePath, orderID)
+	resource := new(TransactionsResource)
+	err := s.client.Get(path, resource, options)
+	return resource.Transactions, err
+}
---
+++ transaction_test.go
@@ -0,0 +1,144 @@
+package goshopify
+
+import (
+	"testing"
+	"time"
+
+	"github.com/shopspring/decimal"
+	httpmock "gopkg.in/jarcoal/httpmock.v1"
+)
+
+func TransactionTests(t *testing.T, transaction Transaction) {
+	// Check that the ID is assigned to the returned transaction
+	expectedID := 389404469
+	if transaction.ID != expectedID {
+		t.Errorf("Transaction.ID returned %+v, expected %+v", transaction.ID, expectedID)
+	}
+
+	// Check that the OrderID value is assigned to the returned transaction
+	expectedOrderID := 450789469
+	if transaction.OrderID != expectedOrderID {
+		t.Errorf("Transaction.OrderID returned %+v, expected %+v", transaction.OrderID, expectedOrderID)
+	}
+
+	// Check that the Amount value is assigned to the returned transaction
+	expectedAmount, _ := decimal.NewFromString("409.94")
+	if !transaction.Amount.Equals(expectedAmount) {
+		t.Errorf("Transaction.Amount returned %+v, expected %+v", transaction.Amount, expectedAmount)
+	}
+
+	// Check that the Kind value is assigned to the returned transaction
+	expectedKind := "authorization"
+	if transaction.Kind != expectedKind {
+		t.Errorf("Transaction.Kind returned %+v, expected %+v", transaction.Kind, expectedKind)
+	}
+
+	// Check that the Gateway value is assigned to the returned transaction
+	expectedGateway := "bogus"
+	if transaction.Gateway != expectedGateway {
+		t.Errorf("Transaction.Gateway returned %+v, expected %+v", transaction.Gateway, expectedGateway)
+	}
+
+	// Check that the Status value is assigned to the returned transaction
+	expectedStatus := "success"
+	if transaction.Status != expectedStatus {
+		t.Errorf("Transaction.Status returned %+v, expected %+v", transaction.Status, expectedStatus)
+	}
+
+	// Check that the Message value is assigned to the returned transaction
+	expectedMessage := "Bogus Gateway: Forced success"
+	if transaction.Message != expectedMessage {
+		t.Errorf("Transaction.Message returned %+v, expected %+v", transaction.Message, expectedMessage)
+	}
+
+	// Check that the CreatedAt value is assigned to the returned transaction
+	expectedCreatedAt := time.Date(2017, time.July, 24, 19, 9, 43, 0, time.UTC)
+	if !expectedCreatedAt.Equal(*transaction.CreatedAt) {
+		t.Errorf("Transaction.CreatedAt returned %+v, expected %+v", transaction.CreatedAt, expectedCreatedAt)
+	}
+
+	// Check that the Test value is assigned to the returned transaction
+	expectedTest := true
+	if transaction.Test != expectedTest {
+		t.Errorf("Transaction.Test returned %+v, expected %+v", transaction.Test, expectedTest)
+	}
+
+	// Check that the Authorization value is assigned to the returned transaction
+	expectedAuthorization := "authorization-key"
+	if transaction.Authorization != expectedAuthorization {
+		t.Errorf("Transaction.Authorization returned %+v, expected %+v", transaction.Authorization, expectedAuthorization)
+	}
+
+	// Check that the Currency value is assigned to the returned transaction
+	expectedCurrency := "USD"
+	if transaction.Currency != expectedCurrency {
+		t.Errorf("Transaction.Currency returned %+v, expected %+v", transaction.Currency, expectedCurrency)
+	}
+
+	// Check that the LocationID value is assigned to the returned transaction
+	var expectedLocationID *int
+	if transaction.LocationID != expectedLocationID {
+		t.Errorf("Transaction.LocationID returned %+v, expected %+v", transaction.LocationID, expectedLocationID)
+	}
+
+	// Check that the UserID value is assigned to the returned transaction
+	var expectedUserID *int
+	if transaction.UserID != expectedUserID {
+		t.Errorf("Transaction.UserID returned %+v, expected %+v", transaction.UserID, expectedUserID)
+	}
+
+	// Check that the ParentID value is assigned to the returned transaction
+	var expectedParentID *int
+	if transaction.ParentID != expectedParentID {
+		t.Errorf("Transaction.ParentID returned %+v, expected %+v", transaction.ParentID, expectedParentID)
+	}
+
+	// Check that the DeviceID value is assigned to the returned transaction
+	var expectedDeviceID *int
+	if transaction.DeviceID != expectedDeviceID {
+		t.Errorf("Transacion.DeviceID returned %+v, expected %+v", transaction.DeviceID, expectedDeviceID)
+	}
+
+	// Check that the ErrorCode value is assigned to the returned transaction
+	var expectedErrorCode string
+	if transaction.ErrorCode != expectedErrorCode {
+		t.Errorf("Transaction.ErrorCode returned %+v, expected %+v", transaction.ErrorCode, expectedErrorCode)
+	}
+
+	// Check that the SourceName value is assigned to the returned transaction
+	expectedSourceName := "web"
+	if transaction.SourceName != expectedSourceName {
+		t.Errorf("Transaction.SourceName returned %+v, expected %+v", transaction.SourceName, expectedSourceName)
+	}
+
+	// Check that the PaymentDetails value is assigned to the returned transaction
+	var nilString string
+	expectedPaymentDetails := PaymentDetails{
+		AVSResultCode:     nilString,
+		CreditCardBin:     nilString,
+		CVVResultCode:     nilString,
+		CreditCardNumber:  "•••• •••• •••• 4242",
+		CreditCardCompany: "Visa",
+	}
+	if transaction.PaymentDetails.AVSResultCode != expectedPaymentDetails.AVSResultCode {
+		t.Errorf("Transaction.PaymentDetails.AVSResultCode returned %+v, expected %+v",
+			transaction.PaymentDetails.AVSResultCode, expectedPaymentDetails.AVSResultCode)
+	}
+}
+
+func TestTransactionList(t *testing.T) {
+	setup()
+	defer teardown()
+
+	httpmock.RegisterResponder("GET", "https://fooshop.myshopify.com/admin/orders/1/transactions.json",
+		httpmock.NewBytesResponder(200, loadFixture("transactions.json")))
+
+	transactions, err := client.Transaction.List(1, nil)
+	if err != nil {
+		t.Errorf("Transaction.List returned error: %v", err)
+	}
+
+	for _, transaction := range transactions {
+		TransactionTests(t, transaction)
+	}
+}
