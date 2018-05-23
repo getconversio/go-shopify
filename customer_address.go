18ce459866a2325b26f7c2aa7523c09d2f5659ef
Jared Fraser <jared.fraser@gmail.com>
2018-05-24 07:25:12 +1000

    Add customer addresses and default address (#87)

---
+++ customer_address.go
@@ -0,0 +1,22 @@
+package goshopify
+
+// CustomerAddress represents a Shopify customer address
+type CustomerAddress struct {
+	ID           int    `json:"id"`
+	CustomerID   int    `json:"customer_id"`
+	FirstName    string `json:"first_name"`
+	LastName     string `json:"last_name"`
+	Company      string `json:"company"`
+	Address1     string `json:"address1"`
+	Address2     string `json:"address2"`
+	City         string `json:"city"`
+	Province     string `json:"province"`
+	Country      string `json:"country"`
+	Zip          string `json:"zip"`
+	Phone        string `json:"phone"`
+	Name         string `json:"name"`
+	ProvinceCode string `json:"province_code"`
+	CountryCode  string `json:"country_code"`
+	CountryName  string `json:"country_name"`
+	Default      bool   `json:"default"`
+}
