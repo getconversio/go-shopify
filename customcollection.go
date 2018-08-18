8a1575bccc93b530aafd59927bfe2ca4725faf76
Kaniel Yano <gir@users.noreply.github.com>
2018-08-18 03:14:30 -0700

    Add metafields to custom and smart collection. (#110)

--- customcollection.go
+++ customcollection.go
@@ -6,6 +6,7 @@ import (
 )
 
 const customCollectionsBasePath = "admin/custom_collections"
+const customCollectionsResourceName = "collections"
 
 // CustomCollectionService is an interface for interacting with the custom
 // collection endpoints of the Shopify API.
@@ -17,6 +18,9 @@ type CustomCollectionService interface {
 	Create(CustomCollection) (*CustomCollection, error)
 	Update(CustomCollection) (*CustomCollection, error)
 	Delete(int) error
+
+	// MetafieldsService used for CustomCollection resource to communicate with Metafields resource
+	MetafieldsService
 }
 
 // CustomCollectionServiceOp handles communication with the custom collection
@@ -27,17 +31,18 @@ type CustomCollectionServiceOp struct {
 
 // CustomCollection represents a Shopify custom collection.
 type CustomCollection struct {
-	ID             int        `json:"id"`
-	Handle         string     `json:"handle"`
-	Title          string     `json:"title"`
-	UpdatedAt      *time.Time `json:"updated_at"`
-	BodyHTML       string     `json:"body_html"`
-	SortOrder      string     `json:"sort_order"`
-	TemplateSuffix string     `json:"template_suffix"`
-	Image          Image      `json:"image"`
-	Published      bool       `json:"published"`
-	PublishedAt    *time.Time `json:"published_at"`
-	PublishedScope string     `json:"published_scope"`
+	ID             int         `json:"id"`
+	Handle         string      `json:"handle"`
+	Title          string      `json:"title"`
+	UpdatedAt      *time.Time  `json:"updated_at"`
+	BodyHTML       string      `json:"body_html"`
+	SortOrder      string      `json:"sort_order"`
+	TemplateSuffix string      `json:"template_suffix"`
+	Image          Image       `json:"image"`
+	Published      bool        `json:"published"`
+	PublishedAt    *time.Time  `json:"published_at"`
+	PublishedScope string      `json:"published_scope"`
+	Metafields     []Metafield `json:"metafields,omitempty"`
 }
 
 // CustomCollectionResource represents the result form the custom_collections/X.json endpoint
@@ -94,4 +99,40 @@ func (s *CustomCollectionServiceOp) Update(collection CustomCollection) (*Custom
 // Delete an existing custom collection.
 func (s *CustomCollectionServiceOp) Delete(collectionID int) error {
 	return s.client.Delete(fmt.Sprintf("%s/%d.json", customCollectionsBasePath, collectionID))
+}
+
+// List metafields for a custom collection
+func (s *CustomCollectionServiceOp) ListMetafields(customCollectionID int, options interface{}) ([]Metafield, error) {
+	metafieldService := &MetafieldServiceOp{client: s.client, resource: customCollectionsResourceName, resourceID: customCollectionID}
+	return metafieldService.List(options)
+}
+
+// Count metafields for a custom collection
+func (s *CustomCollectionServiceOp) CountMetafields(customCollectionID int, options interface{}) (int, error) {
+	metafieldService := &MetafieldServiceOp{client: s.client, resource: customCollectionsResourceName, resourceID: customCollectionID}
+	return metafieldService.Count(options)
+}
+
+// Get individual metafield for a custom collection
+func (s *CustomCollectionServiceOp) GetMetafield(customCollectionID int, metafieldID int, options interface{}) (*Metafield, error) {
+	metafieldService := &MetafieldServiceOp{client: s.client, resource: customCollectionsResourceName, resourceID: customCollectionID}
+	return metafieldService.Get(metafieldID, options)
+}
+
+// Create a new metafield for a custom collection
+func (s *CustomCollectionServiceOp) CreateMetafield(customCollectionID int, metafield Metafield) (*Metafield, error) {
+	metafieldService := &MetafieldServiceOp{client: s.client, resource: customCollectionsResourceName, resourceID: customCollectionID}
+	return metafieldService.Create(metafield)
+}
+
+// Update an existing metafield for a custom collection
+func (s *CustomCollectionServiceOp) UpdateMetafield(customCollectionID int, metafield Metafield) (*Metafield, error) {
+	metafieldService := &MetafieldServiceOp{client: s.client, resource: customCollectionsResourceName, resourceID: customCollectionID}
+	return metafieldService.Update(metafield)
+}
+
+// // Delete an existing metafield for a custom collection
+func (s *CustomCollectionServiceOp) DeleteMetafield(customCollectionID int, metafieldID int) error {
+	metafieldService := &MetafieldServiceOp{client: s.client, resource: customCollectionsResourceName, resourceID: customCollectionID}
+	return metafieldService.Delete(metafieldID)
 }
